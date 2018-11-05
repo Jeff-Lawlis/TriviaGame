ef5c731ec9a9faa6ee7d621c08979e7d82501c6b
Jeff Lawlis <jeff.lawlis@yahoo.com>
2018-11-05 09:31:29 -0600

    fixed win-loss screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,31 +11,35 @@ var trivia = [
         correctAnswer: "something1"
     }
 ]
-var currentQuestion
+var currentQuestion  
 var correct = 0
 var incorrect = 0
-var time = 15
+var time = 5
 var intervalId
+var gameOver = false
 
 function fillButtons(answer){
     button = $("<button>").addClass("btn btn-secondary").text(answer)
     $("#answers-block").append(button)
 }
 function resetQuestion(){
-   checkWin()
-   currentQuestion = trivia[Math.floor(Math.random() * trivia.length)]
-   $(".card-title").text("Question " + parseInt(correct + incorrect + 1))
-   $("#current-question").text(currentQuestion.question)
-   $("#answers-block").html("<span>")
-   $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
-   for(var i = 0; i < currentQuestion.answers.length; i++){
-       fillButtons(currentQuestion.answers[i])
-   }
-   time = 15
-   timerRun()
-   $("button").on("click", function(){
-    checkAnswer(this.innerText, currentQuestion.correctAnswer)
-})
+    checkWin()
+    if(gameOver === false){
+        currentQuestion = trivia[Math.floor(Math.random() * trivia.length)]
+        $(".card-title").text("Question " + parseInt(correct + incorrect + 1))
+        $("#current-question").text(currentQuestion.question)
+        $("#answers-block").html("<span>")
+        $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
+        for(var i = 0; i < currentQuestion.answers.length; i++){
+            fillButtons(currentQuestion.answers[i])
+        }
+        time = 5
+        timerRun()
+        $("button").on("click", function(){
+            checkAnswer(this.innerText, currentQuestion.correctAnswer)
+            checkWin()
+        })
+    }
 }
 function timerRun(){
     clearInterval(intervalId)
@@ -49,6 +53,7 @@ function decrement(){
         time--
         $("#show-timer").text("Time Remaining: " + time)
     } else {
+        checkWin()
         stopTimer()
         renderTimeUp()
     }
@@ -57,8 +62,10 @@ function decrement(){
 function checkAnswer(currentAnswer, correctAnswer){
     if(currentAnswer === correctAnswer){
         renderCorrect()
+        setTimeout(resetQuestion, 3000)
     } else {
         renderIncorrect()
+        setTimeout(resetQuestion, 3000)
     }
 }
 function renderCorrect(){
@@ -66,14 +73,14 @@ function renderCorrect(){
     correct += 1
     $(".card-title").text("Correct!")
     $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
-    setTimeout(resetQuestion, 5000)
+    setTimeout(resetQuestion, 3000)
 }
 function renderIncorrect(){
     stopTimer()
     incorrect += 1
     $(".card-title").text("Incorrect!")
     $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
-    setTimeout(resetQuestion, 5000)
+    setTimeout(resetQuestion, 3000)
 }
 function renderTimeUp(){
     stopTimer()
@@ -83,26 +90,32 @@ function renderTimeUp(){
     setTimeout(resetQuestion, 3000)
 }
 function renderWin(){
+    stopTimer()
     $(".card-title").text("You Win!")
     $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
-    if(confirm("Play again?")){
-        resetQuestion()
-        correct = 0
-        incorrect = 0
-    }
 }
 function renderLoss(){
+    stopTimer()
     $(".card-title").text("You Lose!")
     $("#win-loss").text("Correct: " + correct + " Incorrect: " + incorrect)
 }
 function checkWin(){
     if(correct >=5){
+        gameOver = true
+        clearTimeout()
         stopTimer()
         renderWin()
-    } else {
+    } else if(incorrect >= 5){
+        gameOver = true
+        clearTimeout()
         stopTimer()
         renderLoss()
     }
+}
+function resetGame(){
+    correct = 0
+    incorrect = 0
+    resetQuestion()
 }
 resetQuestion()
 })
